f045646176e756a8b7894ea9c4e87acca64fe53a
Kaushik Samadder <70917815+Kaushik2003@users.noreply.github.com>
2024-07-30 03:24:30 +0530

    Adding zod authetication on types.js and creating a basic boiler plate of express on index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,18 @@
-const a=1133;
+const express=require("express");
+const app=express();
+app.use(express.json());
+
+app.post("/todo",(req,res)={
+
+})
+app.get("/todos",(req,res)={
+    
+})
+app.put("/completed",(req,res)={
+    
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
